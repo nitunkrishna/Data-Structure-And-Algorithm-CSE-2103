00fbab97c95fe585f5898ee1b7a0f175cbb37ab9
Nitun Krishna Biswas <tgnitun05@gmail.com>
2025-11-03 22:25:25 +0600

    Add files via upload
    Here given a code for sorting that has:
    => Bubble Sort
    => Insertion Sort
    => Selection Sort

--- Sorting_Algorithms.cpp
+++ Sorting_Algorithms.cpp
@@ -97,7 +97,7 @@ void selection_sort(float arr[], int n)
     {
         min=i;
         for(int j=i+1; j<n; j++)
-            if(arr[j]<arr[min]) min = j;
+            if(arr[j]<arr[min]) min=j;
         swap(arr[i], arr[min]);
     }
     // Printing the result
